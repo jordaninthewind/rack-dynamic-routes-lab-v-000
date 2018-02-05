51493cc8f1817cbbeb5015dde1bb233abbf87bfc
Jordan Kline <jordan.kline@gmail.com>
2018-02-05 18:33:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,10 +6,18 @@ class Application
       resp = Rack::Response.new
       req = Rack::Request.new(env)
 
-      if req.path=="/items/Figs"
-        resp.write @@items[0].price
-      elsif req.path == "/items/Pears"
-        resp.write @@items[1].price
+      if req.path.match(/items/)
+
+        item_name = req.path.split("/items/").last
+        item = @@items.find{|i| i.name == item_name}
+
+        if @@items.include?(item)
+          resp.write item.price
+        else
+          resp.write "Item not found"
+          resp.status = 400
+        end
+
       else
         resp.write "Route not found"
         resp.status = 404
